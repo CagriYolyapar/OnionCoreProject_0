a3294378a0a9b6127c2f2e3c616821419f6f1731
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-03-07 15:09:21 +0300

    Add project files.

---
+++ Project.BLL/ServiceInjection/DBContextService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.ServiceInjection
+{
+    //DbContextPool'umuz böylece StartUp'da DAL'de bir sınıfı kullanmaktan ziyade sadece BLL'deki bu service Inject edecek
+    public static class DBContextService
+    {
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            //Sakın IConfiguration kullanırken Castle kütüphanesini kullanmayın...Kullanacagınız kütüphane Microsoft.Extensions.Configuration olmak zorundadır...
+
+            IConfiguration configuration = provider.GetService<IConfiguration>();
+
+            services.AddDbContextPool<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+            return services;
+        }
+    }
+}
---
+++ Project.DAL/Repositories/Concretes/AppUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class AppUserRepository : BaseRepository<AppUser>, IAppUserRepository
+    {
+        //Sizin keninize özel crud işlemlerinizin yine olması gerekir...ANcak unutmayın ki Identity yapısının özel şifrelemeler ve yetkilendirmeleri icin hazır async metotları vardır...Bu metotların kullanımı icin de ekstra olarak bu Repository'de ayrı alanlar acmak en dogrusudur...Bu metotlar Manager sınıfları icerisinde bulunur(UserManager,SignInManager hepsi Identity'de gömülü olan sınıflardır)...Bu sınıflar Dependency Injection ile calısırlar...Ve constructor based injection yapılabilir...
+
+
+        UserManager<AppUser> _userManager;
+        SignInManager<AppUser> _signInManager;
+
+        public AppUserRepository(MyContext db, UserManager<AppUser> userManager,SignInManager<AppUser> signInManager) : base(db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        //Özel bir Identity Metodu async seklinde tanımlanmalıdır... Cünkü siz burada sonucta bir API kullanıyorsunuz...Ve bu API requestlerinin bloklanmadan devam edebilmesi icin await keyword'unun kullanılması gerekir... Bir metot icerisinde await keyword'unu kullanabilmeniz icin o metodun async tanımlanması ve Task döndürmesi gerekir...
+
+        public async Task<bool> AddUser(AppUser item)
+        {
+            //Sadece Asenkron olarak yaratılmıs(async marklı) metotlar icerisinde await kullanabilirsiniz...
+
+            IdentityResult result = await _userManager.CreateAsync(item, item.PasswordHash);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(item, isPersistent: false); //isPersistent durumu COokie'de dursun mu durmasın mı
+            }
+
+            return false;
+        }
+    }
+}
